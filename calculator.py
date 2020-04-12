7c32b6c62377e24efceb3a41429d385a91256ba2
Hemanth Anand <HemanthAnand@Hemanths-MacBook-Pro.local>
2020-04-12 23:10:56 +0200

    new script leap

--- calculator.py
+++ calculator.py
@@ -1,33 +1,29 @@
-# This code takes the input from the user
-# and performs the operation selected by the user
-
-def addition(a, b):
-    return a + b
-
-def subtraction(a, b):
-    return a - b
-
-def multiplication(a, b):
-    return a * b
-
-def division(a, b):
-    return a / b
-
-class Calculator:
-    first_input = float(input('Please enter the first number : '))
-    second_input = float(input('Please enter the second number : '))
-    operations = ['+', '-', '*', '/']
-    print('Here are your operator: '+' '.join(operations))
-    operation = input('Please enter the operator : ')
+# put your python code here
+first = float(input())
+second = float(input())
+operators = ['+', '-', '/', '*', 'mod', 'pow', 'div']
+operation = input()
+if operation in operators:
     if operation == '+':
-        result = addition(first_input, second_input)
-        print('Result : ', result)
+        print(first + second)
     elif operation == '-':
-        result = subtraction(first_input, second_input)
-        print('Result : ', result)
+        print(first - second)
     elif operation == '*':
-        result = multiplication(first_input, second_input)
-        print('Result : ', result)
+        print(first * second)
+    elif operation == 'pow':
+        print(first ** second)
+    elif operation == '/':
+        if second == 0:
+            print('Division by 0!')
+        else:
+            print(first / second)
+    elif operation == 'mod':
+        if second == 0:
+            print('Division by 0!')
+        else:
+            print(first % second)
     else:
-        result = division(first_input, second_input)
-        print('Result : ', result)
+        if second == 0:
+            print('Division by 0!')
+        else:
+            print(first // second)
